fb70b0f46a32385b4977f7968a271ab1c64e4161
Donal Wall <91415175+DonalWall207@users.noreply.github.com>
2024-11-05 18:51:20 +0000

    Protected Routes and Clean Up of Classes

--- lib/app-api.ts
+++ lib/app-api.ts
@@ -4,6 +4,12 @@ import { Construct } from "constructs";
 import * as apig from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as node from "aws-cdk-lib/aws-lambda-nodejs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as custom from "aws-cdk-lib/custom-resources";
+import { generateBatch } from "../shared/util";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import { movies } from "../seed/movies";
 
 type AppApiProps = {
   userPoolId: string;
@@ -11,7 +17,6 @@ type AppApiProps = {
 };
 
 export class AppApi extends Construct {
-  public readonly requestAuthorizer: apig.RequestAuthorizer; // Export the authorizer
 
   constructor(scope: Construct, id: string, props: AppApiProps) {
     super(scope, id);
@@ -37,35 +42,217 @@ export class AppApi extends Construct {
       },
     };
 
-    const protectedRes = appApi.root.addResource("protected");
-    const publicRes = appApi.root.addResource("public");
-
-    const protectedFn = new node.NodejsFunction(this, "ProtectedFn", {
-      ...appCommonFnProps,
-      entry: "./lambda/protected.ts",
+     // DynamoDB Tables
+     const moviesTable = new dynamodb.Table(this, "MoviesTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "Movies",
     });
 
-    const publicFn = new node.NodejsFunction(this, "PublicFn", {
-      ...appCommonFnProps,
-      entry: "./lambda/public.ts",
+    const translationsTable = new dynamodb.Table(this, "TranslationsTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "OriginalText", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "TargetLanguage", type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "Translations",
     });
+
+    // Lambda Functions
+    const getMovieByIdFn = new lambdanode.NodejsFunction(
+      this,
+      "GetMovieByIdFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambda/lambdas/rest/getMovieById.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
+    const getAllMoviesFn = new lambdanode.NodejsFunction(
+      this,
+      "GetAllMoviesFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_18_X,
+        entry: `${__dirname}/../lambda/lambdas/rest/getAllMovies.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
+    new custom.AwsCustomResource(this, "moviesddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [moviesTable.tableName]: generateBatch(movies),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [moviesTable.tableArn],
+      }),
+    });
+
+    const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda/lambdas/rest/addMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
+    const deleteMovieFn = new lambdanode.NodejsFunction(this, "DeleteMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambda/lambdas/rest/deleteMovie.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
+    const updateMovieFn = new lambdanode.NodejsFunction(this, "UpdateMovieFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: `${__dirname}/../lambda/lambdas/rest/updateMovie.ts`, 
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: moviesTable.tableName,
+        REGION: "eu-west-1",
+      },
+    });
+
+    const getTranslationFn = new lambdanode.NodejsFunction(
+      this,
+      "GetTranslationFn",
+      {
+        architecture: lambda.Architecture.ARM_64,
+        runtime: lambda.Runtime.NODEJS_16_X,
+        entry: `${__dirname}/../lambda/lambdas/rest/translate.ts`,
+        timeout: cdk.Duration.seconds(10),
+        memorySize: 128,
+        environment: {
+          TABLE_NAME: moviesTable.tableName,
+          REGION: 'eu-west-1',
+        },
+      }
+    );
+
+    const translatePolicyStatement = new iam.PolicyStatement({
+      actions: ["translate:TranslateText"],
+      resources: ["*"],
+    });
+
+    getTranslationFn.addToRolePolicy(translatePolicyStatement);
+
+
+    // Permissions
+    moviesTable.grantReadData(getMovieByIdFn);
+    moviesTable.grantReadData(getAllMoviesFn);
+    moviesTable.grantReadWriteData(newMovieFn);
+    moviesTable.grantReadWriteData(deleteMovieFn);
+    translationsTable.grantReadWriteData(getTranslationFn);  // For the Translations table
+    moviesTable.grantReadWriteData(getTranslationFn); // Adding this line for Movies table access
+    moviesTable.grantReadWriteData(updateMovieFn);
+
+    // REST API
+    const api = new apig.RestApi(this, "RestAPI", {
+      description: "Demo API",
+      deployOptions: {
+        stageName: "dev",
+      },
+      defaultCorsPreflightOptions: {
+        allowHeaders: ["Content-Type", "X-Amz-Date"],
+        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowCredentials: true,
+        allowOrigins: ["*"],
+      },
+    });
+
+    const moviesEndpoint = api.root.addResource("movies");
+    moviesEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getAllMoviesFn, { proxy: true }),
+      
+    );
+
+    const movieEndpoint = moviesEndpoint.addResource("{movieId}");
+    movieEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getMovieByIdFn, { proxy: true }),
+      {
+        
+        requestParameters: {
+          'method.request.querystring.cast': false // Allow optional query parameter
+        },
+        
+      }
+    );
 
     const authorizerFn = new node.NodejsFunction(this, "AuthorizerFn", {
       ...appCommonFnProps,
       entry: "./lambda/lambdas/auth/authorizer.ts",
     });
 
-    this.requestAuthorizer = new apig.RequestAuthorizer(this, "RequestAuthorizer", {
-      identitySources: [apig.IdentitySource.header("cookie")],
-      handler: authorizerFn,
-      resultsCacheTtl: cdk.Duration.minutes(0),
-    });
+    const requestAuthorizer = new apig.RequestAuthorizer(
+      this,
+      "RequestAuthorizer",
+      {
+        identitySources: [apig.IdentitySource.header("cookie")],
+        handler: authorizerFn,
+        resultsCacheTtl: cdk.Duration.minutes(0),
+      }
+    );
 
-    protectedRes.addMethod("GET", new apig.LambdaIntegration(protectedFn), {
-      authorizer: this.requestAuthorizer,
-      authorizationType: apig.AuthorizationType.CUSTOM,
-    });
+    
 
-    publicRes.addMethod("GET", new apig.LambdaIntegration(publicFn));
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true }),
+      
+    );
+
+    movieEndpoint.addMethod(
+      "PUT",
+      new apig.LambdaIntegration(updateMovieFn, { proxy: true }),
+      {
+        authorizer: requestAuthorizer,
+        authorizationType: apig.AuthorizationType.CUSTOM,
+      }
+    );
+
+    movieEndpoint.addMethod(
+      "DELETE",
+      new apig.LambdaIntegration(deleteMovieFn, { proxy: true }),
+    );
+
+    const translateEndpoint = movieEndpoint.addResource("translate");
+    translateEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(getTranslationFn, { proxy: true }),
+    )
+
   }
+
 }
